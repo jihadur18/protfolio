e80f30212a5cb32ae6bec642d544896f1920b557
Md. Jihadur Rahman <98876851+jihadur18@users.noreply.github.com>
2023-01-29 00:53:16 +0600

    upload
    upload

---
+++ portfolio/js/index.js
@@ -0,0 +1,16 @@
+/*var element =document.getElementById("demo");
+element.innerHTML=".navbar-menu";*/
+/*console.log("Script running....")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.navbar-menu ').classList.toggle('sidebarGo')
+    if(document.querySelector('.navbar-menu ').classList.contains('sidebarGo')){
+    document.querySelector('.ham').style.display='inline'
+    document.querySelector('.cross').style.display='none'
+
+    }
+    else{
+        document.querySelector('.cross').style.display='inline'
+        document.querySelector('.ham').style.display='none'  
+    }
+} )*/
